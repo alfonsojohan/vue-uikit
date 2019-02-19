80a65fcbc4a6fef467310ca7118b5f41d1daeb3a
Alfonso Johan <alfonso.johan@unitar.my>
2019-02-19 09:03:07 +0800

    Cleanup

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/**
+ * Font awesome icons we will be using. Only include what you need to reduce the bundle size
+ */
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faBars, faBook, faChevronCircleRight, faAmbulance, faMapMarkerAlt, faCommentAlt} from '@fortawesome/free-solid-svg-icons'
 import { faClock } from '@fortawesome/free-regular-svg-icons'
